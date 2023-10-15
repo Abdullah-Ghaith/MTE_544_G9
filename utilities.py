0c5bbabe2734fe0e90145a8264b901c33b44ae39
Nathan <nathantangca@outlook.com>
2023-10-15 11:29:48 -0400

    Added lab two base files

--- utilities.py
+++ utilities.py
@@ -3,51 +3,54 @@ from math import atan2, asin, sqrt
 M_PI=3.1415926535
 
 class Logger:
+    
     def __init__(self, filename, headers=["e", "e_dot", "e_int", "stamp"]):
+        
         self.filename = filename
 
         with open(self.filename, 'w') as file:
+            
             header_str=""
 
             for header in headers:
                 header_str+=header
                 header_str+=", "
-
+            
             header_str+="\n"
-
+            
             file.write(header_str)
 
 
     def log_values(self, values_list):
 
         with open(self.filename, 'a') as file:
+            
             vals_str=""
-
-            # TODO Part 5: Write the values from the list to the file
-            vals_str = ', '.join([str(element) for element in values_list])
-
+            
+            for value in values_list:
+                vals_str+=f"{value}, "
+            
             vals_str+="\n"
-
+            
             file.write(vals_str)
-
+            
 
     def save_log(self):
         pass
 
 class FileReader:
     def __init__(self, filename):
-
+        
         self.filename = filename
-
-
+        
+        
     def read_file(self):
-
+        
         read_headers=False
 
         table=[]
         headers=[]
         with open(self.filename, 'r') as file:
-            # Skip the header line
 
             if not read_headers:
                 for line in file:
@@ -60,35 +63,58 @@ class FileReader:
 
                     read_headers=True
                     break
-
+            
             next(file)
-
+            
             # Read each line and extract values
             for line in file:
                 values = line.strip().split(',')
-
-                row=[]
-
+                
+                row=[]                
+                
                 for val in values:
                     if val=='':
                         break
                     row.append(float(val.strip()))
 
                 table.append(row)
-
+        
         return headers, table
+    
+    
 
-
-# TODO Part 5: Implement the conversion from Quaternion to Euler Angles
+# TODO Part 3: Implement the conversion from Quaternion to Euler Angles
 def euler_from_quaternion(quat):
     """
     Convert quaternion (w in last place) to euler roll, pitch, yaw.
     quat = [x, y, z, w]
     """
+
     # just unpack yaw
-    siny_cosp = 2 * (quat[3] * quat[2] + quat[0] * quat[1])
-    cosy_cosp = 1 - 2 * (quat[1] * quat[1] + quat[2] * quat[2])
-
-    yaw = atan2(siny_cosp, cosy_cosp) * 180 / M_PI # yaw in degrees
-
     return yaw
+
+
+#TODO Part 4: Implement the calculation of the linear error
+def calculate_linear_error(current_pose, goal_pose):
+        
+    # Compute the linear error in x and y
+    # Remember that current_pose = [x,y, theta, time stamp] and goal_pose = [x,y,theta]
+    # Remember to use the Euclidean distance to calculate the error.
+    error_linear= ...
+
+    return error_linear
+
+#TODO Part 4: Implement the calculation of the angular error
+def calculate_angular_error(current_pose, goal_pose):
+
+    # Compute the linear error in x and y
+    # Remember that current_pose = [x,y, theta, time stamp] and goal_pose = [x,y,theta]
+    # Remember that this function returns the difference in orientation between where the robot currently faces and where it should face to reach the goal
+
+    error_angular = ...
+
+    # Remember to handle the cases where the angular error might exceed the range [-π, π]
+
+    ...
+    
+    return error_angular
